32e4948d830e9a1a77544c91f3f23b3c6146d9fc
Vyacheslav Kozlov <53976826+sudden-ignition@users.noreply.github.com>
2020-11-11 11:43:38 +0500

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,8 +1,7 @@
 public class FizzBuzz{
 
     public static void main(String[] args) {
-        System.out.println("1"); //in that case - this is the easiest way to solve
-        System.out.println("2");
+
         for (int i = 3; i <= 1000; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
